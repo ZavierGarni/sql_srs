683e1f578fbdbaae7e0cdb8ec14e97f58baa0cf4
Xav <xavier.garni@live.fr>
2024-04-23 14:29:14 +0200

    ajout sidebar

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ st.write("Hello World !")
 data = {"a": [1, 2, 3], "b": [4, 5, 6]}
 df = pd.DataFrame(data)
 
-option = st.selectbox(
-    "What would you like to review",
-    ("Joins", "GroupBy", "Window Functions"),
-    index=None,
-    placeholder="Select a topic..."
-)
-
-st.write("You have selected : ", option)
+with st.sidebar:
+    option = st.selectbox(
+        "What would you like to review",
+        ("Joins", "GroupBy", "Window Functions"),
+        index=None,
+        placeholder="Select a topic..."
+    )
+    st.write("You have selected : ", option)
 
 input_text = st.text_area(label='Entrez votre input')
 st.write(f"Vous avez entré : {input_text}")
