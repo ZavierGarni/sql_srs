66c083768fe56915990ecfe01c631a030db85f79
Xav <xavier.garni@live.fr>
2024-04-23 16:43:26 +0200

    reallyyy

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ df = pd.DataFrame(data)
 
 with st.sidebar:
     option = st.selectbox(
-        "What topic are REALLY you interested in ?",
+        "What topic are REALLYYY you interested in ?",
         ("Joins", "GroupBy", "Window Functions"),
         index=None,
         placeholder="Select a topic..."
